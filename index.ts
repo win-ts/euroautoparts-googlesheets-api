779aed1cded7646fa4aae41cb6434c412513401b
Win-TS <winthanatpong@gmail.com>
2024-05-24 23:27:23 +0700

    feat: connect google sheets and create endpoints

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import  * as Service from "./service";
+
+const server = Bun.serve({
+    port: process.env.PORT,
+    async fetch (req) {
+        const path = new URL(req.url).pathname;
+
+        // GET all products from Google Sheets
+        if (req.method === "GET" && path === "/products") {
+            const products = await Service.getProducts();
+            return Response.json({ success: true, products });
+        }
+    
+        // POST new lead to Google Sheets
+        if (req.method === "POST" && path === "/lead") {
+            const payload = await req.formData();
+            const newLead = await Service.newLead(payload);
+            return Response.json({ success: true, newLead });
+        }
+
+        if (req.method === "GET" && path === "/products/best-selling") {
+            const products = await Service.getBestSellingProducts();
+            return Response.json({ success: true, products });
+        }
+
+        return new Response("Page not found", { status: 404 });
+    }
+})
+
+console.log(`Listening on ${server.url}`);
---
+++ service.ts
@@ -0,0 +1,74 @@
+import { google } from 'googleapis';
+
+export const getProducts = async () => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const googleSheets = google.sheets({ version: "v4", auth: auth });
+    const spreadsheetId = process.env.SPREADSHEET_ID ?? "";
+
+    try {
+        const response = await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId,
+            range: 'Products!A2:H',
+        });
+        const result = response.data;
+        return result.values;
+    } catch (err) {
+        console.error('Error retrieving spreadsheet data:', err);
+        return err;
+    }
+}
+
+export const newLead = async (payload: FormData) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const googleSheets = google.sheets({ version: "v4", auth: auth });
+    const spreadsheetId = process.env.SPREADSHEET_ID ?? "";
+
+    try {
+        const response = await googleSheets.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: 'Leads!A:C',
+            valueInputOption: 'RAW',
+            requestBody: {
+                values: [
+                    [payload.get("name"), payload.get("contact"), payload.get("phone")]
+                ]
+            },
+            includeValuesInResponse: true,
+        });
+        const result = response.data;
+        return result;
+    } catch (err) {
+        console.error('Error retrieving spreadsheet data:', err);
+        return err;
+    }
+}
+
+export const getBestSellingProducts = async () => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const googleSheets = google.sheets({ version: "v4", auth: auth });
+    const spreadsheetId = process.env.SPREADSHEET_ID ?? "";
+
+    try {
+        const response = await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId,
+            range: 'BestSellingProducts!A2:D',
+        });
+        const result = response.data;
+        return result.values;
+    } catch (err) {
+        console.error('Error retrieving spreadsheet data:', err);
+        return err;
+    }
+}
