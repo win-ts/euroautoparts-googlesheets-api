d9050cd3a09079626dc09c19ab2bb06fdfd09094
win-ts <winthanatpong@gmail.com>
2024-09-25 00:45:15 +0700

    feat: init k8s manifest files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(express.json());
